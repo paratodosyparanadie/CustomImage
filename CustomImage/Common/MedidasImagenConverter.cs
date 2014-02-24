9eb29afa8f2b5fbbdc51bcf3c7b076697a4fee6e
paratodosyparanadie <decilo@paratodosyparanadie.com>
2014-02-24 15:01:37 -0300

    Agregar un controlador, converter en el style

---
+++ CustomImage/Common/MedidasImagenConverter.cs
@@ -0,0 +1,36 @@
+﻿namespace CustomImage.Common
+{
+    using System;
+    using Windows.Storage.FileProperties;
+    using Windows.UI.Xaml.Data;
+    using Windows.UI.Xaml.Media;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Media.Imaging;
+    class MedidasImagenConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            BitmapImage myBitmapImage = value as BitmapImage;
+            string myuri = myBitmapImage.UriSource.ToString();
+            char[] separator = new char[] { '_' };
+            string[] measures = myuri.Split(separator, StringSplitOptions.None);
+            double width = 310.0;      
+            if (measures.Length > 1) {
+                double oWidth = double.Parse(measures[1], System.Globalization.CultureInfo.InvariantCulture);
+                double oHeigth = double.Parse(measures[2], System.Globalization.CultureInfo.InvariantCulture);
+                if (oHeigth > oWidth)
+                {
+                    width = 193.0;
+                }
+            }                
+            
+            return (width);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+          throw new NotImplementedException();
+        }
+
+        
+  }
+}
